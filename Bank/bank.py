82013e03e0af15a1ae220262ac2f1b4b26bd504b
fungfupie <thefungfupie@gmail.com>
2014-10-10 10:35:52 -0400

    reverts commit e5469277b64445bd67114d69f7e7efbe9a551b89.

--- Bank/bank.py
+++ Bank/bank.py
@@ -1,12 +1,1 @@
-#Simple banking program to show use of classes in python#
-class bankAccount:
-	def createAccount(self):
-		self.balance = 0
-	
-	def withdraw(self, amount):
-		self.balance -= amount
-		return self.balance
-
-	def deposit(self, amount):
-		self.balance += amount
-		return self.balance
+print "hello"
