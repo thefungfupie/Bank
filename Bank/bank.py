24de6156ef1d2f6363d04b94f84cf0a5cf0523d8
fungfupie <thefungfupie@gmail.com>
2014-10-10 10:49:39 -0400

    bank code

--- Bank/bank.py
+++ Bank/bank.py
@@ -1,1 +1,15 @@
-print "hello"
+#Simple Python banking program
+
+class BankAccount:
+	def create(self):
+		self.balance=0
+
+	def withdraw(self, amount):
+		self.balance -= amount
+		return self.balance
+
+	def deposit(self, amount):
+		self.balance += amount
+		return self.balance
+
+
