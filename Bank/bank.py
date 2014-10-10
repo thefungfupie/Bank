e5469277b64445bd67114d69f7e7efbe9a551b89
fungfupie <thefungfupie@gmail.com>
2014-10-10 10:27:30 -0400

    bank program

--- Bank/bank.py
+++ Bank/bank.py
@@ -1,1 +1,12 @@
-print "hello"
+#Simple banking program to show use of classes in python#
+class bankAccount:
+	def createAccount(self):
+		self.balance = 0
+	
+	def withdraw(self, amount):
+		self.balance -= amount
+		return self.balance
+
+	def deposit(self, amount):
+		self.balance += amount
+		return self.balance
